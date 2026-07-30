f461330ea5270fb350a38bd505d2d765c2e9ca50
Abdul-Salam Zakaria <151104581+Abdulsz@users.noreply.github.com>
2026-07-30 16:30:51 -0400

    Add: binary-tree-longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/binary-tree-longest-consecutive-sequence/submission-3.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def longestConsecutive(self, root: Optional[TreeNode]) -> int:
+        self.res = 0
+        def dfs(node):
+
+            if not node:
+                return 0
+
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            curr = 1
+
+            if node.left and node.left.val == node.val+1:
+                curr = max(curr,left + 1)
+            if node.right and node.right.val == node.val+1:
+                curr = max(curr,right + 1)
+
+            self.res = max(self.res,curr)
+            return curr
+
+        dfs(root)
+        return self.res
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
