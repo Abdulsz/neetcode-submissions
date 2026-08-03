83c92c08664b34824b046164ffb5a3bf8417bd80
Abdul-Salam Zakaria <151104581+Abdulsz@users.noreply.github.com>
2026-08-03 08:54:59 -0400

    Add: permutations-ii - submission-2

---
+++ Data Structures & Algorithms/permutations-ii/submission-2.kt
@@ -0,0 +1,32 @@
+class Solution {
+    fun permuteUnique(nums: IntArray): List<List<Int>> {
+        var res = mutableListOf<List<Int>>()
+        var temp = mutableListOf<Int>()
+        var used = BooleanArray(nums.size)
+
+        nums.sort()
+
+        fun backtrack(){
+
+            if(temp.size == nums.size){
+                res.add(temp.toList())
+                return 
+            }
+            for(j in 0 until nums.size){
+                if (j>0 && nums[j] == nums[j-1] &&!used[j-1]){
+                    continue
+                }
+                if (!used[j]){
+                    used[j] = true
+                    temp.add(nums[j])
+                    backtrack()
+                    temp.removeAt(temp.size-1)
+                    used[j] = false
+                }
+            }
+            
+        }
+        backtrack()
+        return res
+    }
+}
